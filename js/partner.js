c59fca60d7d1da06fb13995776592e82da988c21
trungtran1211 <thanhtrungtran8888@gmail.com>
2023-03-01 17:34:02 +0700

    1/3

--- js/partner.js
+++ js/partner.js
@@ -1,29 +1,50 @@
-var swiper = new Swiper(".swiper1", {
+const dynu1 = window.matchMedia('(min-width: 1440px)')
+if (dynu1.matches) {
+  var swiper = new Swiper(".swiper_dynu", {
     effect: 'coverflow',
       grabCursor: true,
       centeredSlides: true,
-      slidesPerView: 'auto',
       coverflowEffect: {
         rotate: 0,
-        stretch: 200,
-        depth: 400,
+        stretch: 290,
+        depth: 300,
         modifier: 1,
-        slideShadows: false,
+        slideShadows: true,
       },
     loop: true,
     breakpoints: {
-      320: {
-        slidesPerView: 1
-      },
-      768: {
-        slidesPerView: 2
-      },
-      1024: {
-        slidesPerView: 2
-      },
       1440: {
         slidesPerView: 2
       }
     }
   });
+}
+const dynu2 = window.matchMedia('(max-width: 1025px)')
+if (dynu2.matches) {
+  var swiper = new Swiper(".swiper_dynu", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 200,
+      modifier: 2,
+      slideShadows: true
+    },
+    loop: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1.2
+      },
+      768: {
+        slidesPerView: 1.5
+      },
+      1024: {
+        slidesPerView: 1.8
+      },
+    }
+  });
+}
+
   
